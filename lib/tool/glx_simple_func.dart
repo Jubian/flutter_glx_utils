99d3615966bbcfe0b0df62b389f19901637b3837
高志凯 <120520885@qq.com>
2023-04-21 02:29:48 +0800

    添加快捷方法：条件判断执行方法

--- lib/tool/glx_simple_func.dart
+++ lib/tool/glx_simple_func.dart
@@ -1,9 +1,10 @@
 
 import 'package:flutter/material.dart';
 
-EdgeInsets glx_ei_a(double value) => EdgeInsets.all(value);
-EdgeInsets glx_ei_s(double horizontal,double vertical) => EdgeInsets.symmetric(horizontal: horizontal,vertical: horizontal);
-EdgeInsets glx_ei_sh(double h) => EdgeInsets.symmetric(horizontal: h);
-EdgeInsets glx_ei_sv(double v) => EdgeInsets.symmetric(vertical: v);
-EdgeInsets glx_ei_zero() => EdgeInsets.zero;
-EdgeInsets glx_ei_ltrb({double left = 0.0,double top = 0.0,double right = 0.0, double bottom = 0.0}) => EdgeInsets.only(left:left, top:top, right:right, bottom:bottom);
+ValueChanged<T>? runfuncIf<T>(bool condition, ValueChanged<T> func) {
+  if (condition) {
+    return func;
+  } else {
+    return null;
+  }
+}
